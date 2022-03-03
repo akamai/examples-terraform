1d2771244d0811b415709f0d1528da529c859165
Mike Elissen <61023476+securitylevelup@users.noreply.github.com>
2022-03-03 11:15:40 +0100

    Add Ion
    Add Ion

---
+++ Ion/modules.tf
@@ -0,0 +1,26 @@
+# modules.tf
+# version 1.0.0
+# This Terraform configuration functions as the configuration to create Akamai modules which acts as logical groupings of Akamai functionality.
+
+module "akamai-property" {
+    source = "./akamai-property"
+
+    edgerc_config_section = var.edgerc_config_section
+
+    contract_id = var.contract_id
+    group_name = var.group_name
+    product_id = "prd_Fresca" #prd_Fresca = Akamai Ion Standard. Do not change.
+
+    hostname = "" #your hostname to Akamaize.
+    edge_hostname = "" #the Akamai Edge Hostname to create, ending in either *.akamaized.net, *.edgesuite.net or *.edgekey.net.
+    origin_hostname = ""
+
+    cpcode_name = ""
+
+    cert_provisioning_type = "CPS_MANAGED" 
+    ip_behavior = "IPV6_COMPLIANCE"
+    rule_format = "latest"
+
+    akamai_network = "STAGING" #STAGING or PRODUCTION
+    email = ""
+}
